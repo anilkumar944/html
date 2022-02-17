364765b7264aee7473c353b9fe80a6593a5320f2
Anil <banilbehera494@gmail.com>
2022-02-17 20:29:57 +0530

    flipkart.html added by don

---
+++ flipkart/category.js
@@ -0,0 +1,25 @@
+categoryList = [{
+    "name": "offer",
+    "img": "https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100",
+    "url": "#"
+}, {
+    "name": "grocery",
+    "img": "https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100",
+    "url": "grocery.html"
+}, {
+    "name": "mobile",
+    "img": "https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100",
+    "url": 'mobile.html'
+}, {
+    "name": "travel",
+    "img": "https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100",
+    "url": '#'
+}, {
+    "name": "electronics",
+    "img": "https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100",
+    "url": 'electronics.html'
+}, {
+    "name": "travel",
+    "img": "https://rukminim1.flixcart.com/flap/128/128/image/82b3ca5fb2301045.png?q=100",
+    "url": '#'
+}]
